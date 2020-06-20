1f11f61370b4eea9a6f0042b1b2109beb11c62a4
KrisNascimento <61701788+KrisNascimento@users.noreply.github.com>
2020-06-19 21:19:24 -0300

    Prova

---
+++ Prova/P0030481923043/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Prova
+{
+    partial class frmProva
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVerifica = new System.Windows.Forms.Button();
+            this.lstbxTotais = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnVerifica
+            // 
+            this.btnVerifica.Font = new System.Drawing.Font("MS Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerifica.Location = new System.Drawing.Point(62, 96);
+            this.btnVerifica.Name = "btnVerifica";
+            this.btnVerifica.Size = new System.Drawing.Size(212, 90);
+            this.btnVerifica.TabIndex = 0;
+            this.btnVerifica.Text = "CALCULAR";
+            this.btnVerifica.UseVisualStyleBackColor = true;
+            this.btnVerifica.Click += new System.EventHandler(this.btnVerifica_Click);
+            // 
+            // lstbxTotais
+            // 
+            this.lstbxTotais.FormattingEnabled = true;
+            this.lstbxTotais.Location = new System.Drawing.Point(358, 13);
+            this.lstbxTotais.Name = "lstbxTotais";
+            this.lstbxTotais.Size = new System.Drawing.Size(389, 329);
+            this.lstbxTotais.TabIndex = 1;
+            // 
+            // frmProva
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstbxTotais);
+            this.Controls.Add(this.btnVerifica);
+            this.Name = "frmProva";
+            this.Text = "Prova";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerifica;
+        private System.Windows.Forms.ListBox lstbxTotais;
+    }
+}
+
---
+++ Prova/P0030481923043/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Collections;
+
+namespace Prova
+{
+    public partial class frmProva : Form
+    {
+        public frmProva()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerifica_Click(object sender, EventArgs e)
+        {
+            lstbxTotais.Items.Clear();
+            double[,] vendas = new double[3, 4];
+            double totalMes, totalGeral;
+            string valor;
+            
+
+            totalGeral = 0;
+            for (int lin = 0; lin < 4; lin++)
+            {
+                totalMes = 0;
+                for (int col = 0; col < 4; col++)
+                {
+                    valor = Interaction.InputBox("Informe o total de vendas da semana " + (col + 1) + " do mês " + (lin + 1) + ":", "Entrada das Valores");
+                    if (double.TryParse(valor, out vendas[lin, col]))
+                    {
+                        lstbxTotais.Items.Add("Total do mês:" + (lin + 1) + " Semana:" + (col + 1) + " R$" + vendas[lin, col].ToString("N2"));
+                        totalMes += vendas[lin, col];
+                    }
+                    else
+                    {
+                        MessageBox.Show("Digite valores válidos!!!");
+                        col--;
+                    }
+                }
+                lstbxTotais.Items.Add(">> Total Mês:" + "R$" + totalMes.ToString("N2"));
+                lstbxTotais.Items.Add("-------------------------------------");
+                totalGeral += totalMes;
+            }
+            lstbxTotais.Items.Add(">> Total Geral:" + "R$" + totalGeral.ToString("N2"));
+
+        }
+    }
+}
